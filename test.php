fe262736e743c816e8a1a24fa726bbb775c0a761
Fawwaz <manusiacoding29@gmail.com>
2021-10-23 11:08:28 +0700

    add timezone

--- test.php
+++ test.php
@@ -1,4 +1,36 @@
 <?php
-$date = new DateTime("now", new DateTimeZone('Asia/Jakarta') );
-echo $date->format('H:i:s');
+// $date = new DateTime("now", new DateTimeZone('Asia/Jakarta') );
+// echo $date->format('H:i:s');
 ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <body onload=display_ct();>
+    <span id='ct' ></span>
+</body>
+</html>
+
+<script>
+    function display_c(){
+        var refresh=1000; // Refresh rate in milli seconds
+        mytime=setTimeout('display_ct()',refresh);
+    }
+    function display_ct() {
+        var x = new Date()
+        var hour=x.getHours();
+        var minute=x.getMinutes();
+        var second=x.getSeconds();
+        if(hour <10 ){hour='0'+hour;}
+        if(minute <10 ) {minute='0' + minute; }
+        if(second<10){second='0' + second;}
+        var x3 = hour+':'+minute+':'+second
+        document.getElementById('ct').innerHTML = x3;
+        display_c();
+    }
+</script>
